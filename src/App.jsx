2904801d403f60dcdabebd20a5a61c65ab335e06
Aditya raj <adityaraj987456@gmail.com>
2025-10-30 21:04:07 +0530

    role switch dropdown added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import { useState } from "react"
+import { SelectNewRole } from "./components/SelectRoleDropdown"
 
 function App() {
 
 
+  const [ role, setRole ]=useState('student')
+
+
+  //function for changing dashboard according to the role selected--> it will runs when role will change.
+  function HandleRoleChange(newSelectedRole) {
+    setRole(newSelectedRole)
+  }
+
+
   return (
-<div>
-  <h1 className="bg-green-300">hello ji</h1>
-</div>
+    <div>
+      <h1 className="bg-green-300">hello ji</h1>
+      <SelectNewRole OnRoleGetsChanged={HandleRoleChange}/>
+    </div>
   )
 }
 
---
+++ src/components/SelectRoleDropdown.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react"
+
+export function SelectNewRole({OnRoleGetsChanged}) {
+
+    const [ role, setRole ]=useState(localStorage.getItem('role') || 'student')   //if first time then by default student
+
+    useEffect( ()=>{
+        localStorage.setItem("role", role)
+        OnRoleGetsChanged(role)    //--> ab main app.jsx ke handlerolechange ko call karega.
+    }, [role])
+
+    return(
+        <div>
+            <select
+                value={role}
+                onChange={(e) => setRole(e.target.value)}
+                className="p-2.5 border border-gray-300 rounded-lg bg-white text-gray-800 font-medium focus:ring-2 focus:ring-blue-500 focus:border-blue-500 hover:border-blue-400 cursor-pointer shadow-sm transition-all duration-200 ease-in-out"
+                >
+                    <option  value="admin">🧑‍💼 Admin</option>
+                    
+                {/* <optgroup label="🎓 Students">
+                    <option value="student1">Student 1</option>
+                    <option value="student2">Student 2</option>
+                    <option value="student3">Student 3</option>
+                    <option value="student4">Student 4</option>
+                </optgroup> */}
+
+                <option value="student">🎓 Student</option>
+
+            </select>
+
+        </div>
+    )
+
+    
+}
