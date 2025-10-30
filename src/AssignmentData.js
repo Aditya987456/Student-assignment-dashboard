90ca9afafaca4fa15584dd19ec39b97ac0487aa8
Aditya raj <adityaraj987456@gmail.com>
2025-10-30 17:04:04 +0530

    added the content of the database like things like students list and assignments

---
+++ src/AssignmentData.js
@@ -0,0 +1,25 @@
+
+//-- all students --
+
+ export const students = [ "student1", "student2", "student3", "student4"]
+
+
+
+
+ //-- all assignments list here --(arr of obj will increase later on addding assign)
+ export const assignments = [
+    {
+        id:'default',
+        title:'default assignmet',
+        dueDate:'12-12-2025',
+        driveLink:'',
+        submitted_Students:{
+            student1:true,
+            student2:true,
+            student3:false,
+            student4:true,
+        }
+    }
+ ]
+
+
